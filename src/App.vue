fb452ae162480ddd9e987f3e1af44fd1e4e39c98
fei <allen.tian@x2x.com>
2018-03-20 16:32:50 +0800

    模块化开发搭建

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+    <div id="app">
+        <SLJHead></SLJHead>
+        <component :is="curView"></component>
+        <ul class="foot">
+            <li v-for="(item, index) in tabs" @click="toggleView(index, item.view)">{{item.text}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import SLJHead from './components/slj-head';
+import SLJTab1 from './components/slj-tab1';
+import SLJTab2 from './components/slj-tab2';
+import SLJTab3 from './components/slj-tab3';
+import SLJTab4 from './components/slj-tab4';
+export default {
+    name: 'App',
+    data(){
+        return {
+            active: 0,
+            curView: 'SLJTab1',
+            tabs: [{
+                text: 'tab1',
+                view: 'SLJTab1'
+            },{
+                text: 'tab2',
+                view: 'SLJTab2'
+            },{
+                text: 'tab3',
+                view: 'SLJTab3'
+            },{
+                text: 'tab4',
+                view: 'SLJTab4'
+            }]
+        }
+    },
+    methods: {
+        toggleView(i, v){
+            this.$store.commit('increment');
+            console.log(this.$store.state.count);
+            this.active = i;
+            this.curView = v;
+        }
+    },
+    components: {
+        SLJHead,SLJTab1,SLJTab2,SLJTab3,SLJTab4
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+    *{ margin: 0; padding: 0; }
+    .foot{ list-style: none; position: absolute; bottom: 0; left: 0; right: 0; height: 50px; }
+    .foot li{ width: 25%; float: left; height: 50px; line-height: 50px; }
+</style>
---
+++ src/components/slj-tab3.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>tab3的内容</div>
+</template>
+<script>
+    export default {
+        
+    }
+</script>
+<style type="text/css">
+    *{ margin: 0; padding: 0; }
+    .foot{ list-style: none; position: absolute; bottom: 0; left: 0; right: 0; height: 50px; }
+    .foot li{ width: 25%; float: left; height: 50px; line-height: 50px; }
+</style>
