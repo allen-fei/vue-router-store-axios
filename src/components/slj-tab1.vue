d9c7b0ca09d587511c4027264e5923e1990b9bbc
fei <allen.tian@x2x.com>
2018-03-22 15:09:26 +0800

    新增弹窗组件 v-m-layer

--- src/components/slj-tab1.vue
+++ src/components/slj-tab1.vue
@@ -19,6 +19,9 @@
             let obj = {"username":"18607318081","password":"e10adc3949ba59abbe56e057f20f883e","sign":"HSoEeFMggcSolDCGmCSQCK1NCgn8dI2N4qhX0R5NltB/3NIf4p3Vgom5u08EI/D6b8rPfXm47CelX4/C49DikrxEtxn3yI07EFnKhThEHHvUlHuTcWLyQnII4o/ebDWkx+CXnLHlEsUh9onhHeLYS5VQn0QqkTOl57NiYWDyOr7v7XIn846iripMQsGZxvUHhhy5hRx7FfzDNzLCn5U6/WFR3H7JB57QmxzIN10tSKX0/vKvmyZNRtA6iynLikjgRYZm6p2w6ExqprabNqaGSq1FVCr0fjgsZKCdGlMdPWs5htcNdnq4HNKibUmv9qCfZtXNeRO2VlY88Pd2Th1waA==","phoneModel":"MI MAX 2","phoneUuid":"865902032595267,865902032595275","getuicid":"1db874edab328772229ee8e1eb6088aa"}
             vueAjax(apis.login, obj).then((res) => {
                 console.log(res);
+                this.$layer.alert('sssss',function(){
+                    console.log('你点击了确定')
+                })
             }, (error) => {
                 console.log(error);
             })
--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import { AlertPlugins } from 'v-m-layer'
+import 'v-m-layer/dist/layer.css'
 import './assets/css/reset.css'
 
 Vue.config.productionTip = false
+
+Vue.use(AlertPlugins)
 
 /* eslint-disable no-new */
 new Vue({
